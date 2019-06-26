3fdcfeb80d2222b44b07b87229d1d5490e71ac1b
Slinkter <luis.j.cueva@gmail.com>
2019-06-26 14:47:58 -0500

    Login diseño : ok

--- app/src/main/java/com/vidrieriachaloreyes/app/LoginActivity.java
+++ app/src/main/java/com/vidrieriachaloreyes/app/LoginActivity.java
@@ -3,12 +3,12 @@ package com.vidrieriachaloreyes.app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }

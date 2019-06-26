976891b70181e47e770c04343b8c08a5675cb03c
Slinkter <luis.j.cueva@gmail.com>
2019-06-26 17:32:35 -0500

    Login Desing and Register ok

--- app/src/main/java/com/vidrieriachaloreyes/app/LoginActivity.java
+++ app/src/main/java/com/vidrieriachaloreyes/app/LoginActivity.java
@@ -1,14 +1,73 @@
 package com.vidrieriachaloreyes.app;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    //
+    private Button btnIngresar;
+    private Button btnRegistrar;
+    //
+    private Animation animation;
+    private Vibrator vib;
+    private FirebaseAuth auth;
+    //
+    private EditText ed_login_email;
+    private EditText ed_login_pwd;
+    private CheckBox checkbox_session;
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+    public static final String PREF_NAME = "prefs";
+    public static final String KEY_REMEMBER = "remeber";
+    public static final String KEY_USERNAME = "username";
+    public static final String KEY_PASS = "password";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
+        //
+        animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.shake);
+        vib = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        //
+        ed_login_email = findViewById(R.id.et_mail);
+        ed_login_pwd = findViewById(R.id.et_password);
+        checkbox_session = findViewById(R.id.checkbox_session);
+        btnIngresar = findViewById(R.id.btnIngresar);
+        btnRegistrar = findViewById(R.id.btnRegistrar);
+
+        btnIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
     }
+
+    // metodo de guardar usuario y contraseña
+
+
 }

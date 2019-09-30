979058a04d088cd82cf2db385ea080d0108be2e6
Slinkter <luis.j.cueva@gmail.com>
2019-09-29 19:05:48 -0500

    demo terminado

--- app/src/main/java/com/vidrieriachaloreyes/app/Fragment/F5.java
+++ app/src/main/java/com/vidrieriachaloreyes/app/Fragment/F5.java
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.vidrieriachaloreyes.app.R;
 
@@ -14,17 +17,34 @@ import com.vidrieriachaloreyes.app.R;
  */
 public class F5 extends Fragment {
 
+    Button btn_Cliente_Ingresar;
+    TextView campo1,campo2;
 
     public F5() {
-        // Required empty public constructor
+
     }
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_f5, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,      Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.fragment_f5, container, false);
+
+        campo1 = view.findViewById(R.id.et_mail_f5);
+        campo2 = view.findViewById(R.id.et_password_f5);
+
+        btn_Cliente_Ingresar = view.findViewById(R.id.btn_Cliente_Ingresar);
+        btn_Cliente_Ingresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (campo1.getText().toString().isEmpty() & campo2.getText().toString().isEmpty()){
+                    Toast.makeText(view.getContext(), "Usuario y contraeña incorrecto", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(view.getContext(), "Usuario no autorizado", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+        return view;
     }
 
 }

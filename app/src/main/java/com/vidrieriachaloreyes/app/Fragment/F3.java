91244f76fda11d8204bde46d8f6fc2d6a8e8d291
Slinkter <luis.j.cueva@gmail.com>
2019-10-04 13:56:08 -0500

    Viernes

--- app/src/main/java/com/vidrieriachaloreyes/app/Fragment/F3.java
+++ app/src/main/java/com/vidrieriachaloreyes/app/Fragment/F3.java
@@ -9,21 +9,17 @@ import android.view.ViewGroup;
 
 import com.vidrieriachaloreyes.app.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+
 public class F3 extends Fragment {
 
 
     public F3() {
-        // Required empty public constructor
+
     }
 
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,                             Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_f3, container, false);
     }
 

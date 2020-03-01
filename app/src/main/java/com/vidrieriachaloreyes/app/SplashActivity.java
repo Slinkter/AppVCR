b15800d942cb488e24cc3e6ff3570292b96c2f5a
Slinkter <luis.j.cueva@gmail.com>
2020-03-01 12:22:34 -0500

    nuevo logo y fragmen de ubicacion

--- app/src/main/java/com/vidrieriachaloreyes/app/SplashActivity.java
+++ app/src/main/java/com/vidrieriachaloreyes/app/SplashActivity.java
@@ -9,7 +9,7 @@ import java.util.TimerTask;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private static final long SPLASH_SCREEN_DELAY = 200;
+    private static final long SPLASH_SCREEN_DELAY = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
